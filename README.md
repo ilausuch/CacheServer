4d824dd6edd1afd6bd6ca656faee27ee820fc54a
ilausuch <ilausuch@mbp-de-ivan>
2017-06-13 22:56:08 +0200

    Config file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # CacheServer
-Multi-thread Cache system
+
+Multi-thread Cache system based on Banks of entities. A bank is a group of entities.
+
+## License
+
+LICENSE MIT @2017 Ivan LAusuch <ilausuch@gmail.com>
+
+## Featuers
+
+* Unlimited entries per bank
+* Unlimited banks
+* Entries with optional lifetime
+* Number of connections only limited by system
+* Event-driven and non blocking server for minimization of CPU usage
+* Multi-thread pool of workers with deathlock protection
+
+## Run server
+
+### Configuration file
+
+A config.json file is required
+
+
+
+## API
+
+### Put
+
+### Get
--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ __date__ = "$12-jun-2017 20:14:15$"
 
 import sys
 import pyev
+import json
 from Queue import Queue
 from Core.Server import Server
 from Core.Worker import Worker
@@ -25,8 +26,31 @@ WORKERS = 4
 SERVER_IP = "127.0.0.1"
 SEVER_PORT = 10001
 
+
+    
 def main():
     
+    try:
+        f=file('config.json')
+        data=f.read()
+        
+        try:
+            cfg=json.loads(data)
+            try:
+                WORKERS = cfg["WORKERS"]
+                SERVER_IP = cfg["SERVER_IP"]
+                SEVER_PORT = cfg["SERVER_PORT"]
+            except:
+                print "Required WORKERS, SERVER_IP and SERVER_PORT in config.json"
+                return
+        except:
+            print "config.json must be a json"
+            return
+    except:
+        print "Requied a valid config.json file"
+        return
+        
+
     # Create job queue
     jobQueue = Queue()
     
