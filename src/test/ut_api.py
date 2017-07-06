c4b07abdcb5b8139ef3317713fb4f8c147971aed
ilausuch <ilausuch@gmail.com>
2017-07-06 15:53:49 +0200

    Added ut_api

---
+++ src/test/ut_api.py
@@ -0,0 +1,112 @@
+'''
+	Multi-thread Cache system
+	LICENSE MIT @2017 Ivan LAusuch <ilausuch@gmail.com>
+    API Test
+'''
+
+import unittest
+import json
+import requests
+
+
+def checkInList(result, item):
+    js = json.loads(result)
+    if js["status"] != 'ok':
+        return False
+
+    return item in js["data"]
+
+
+def checkError(result):
+    js = json.loads(result)
+    return js["status"] == 'error'
+
+
+class Ut_serverTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_api_up(self):
+        r = requests.get('http://localhost:8000/api/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_001_set(self):
+        r = requests.post('http://localhost:8000/api/bank/bank1/entry/entry1',
+                          headers={"Content-Type": "text / plain"},
+                          data="value 1")
+        self.assertEqual(r.status_code, 200)
+
+    def test_002_get(self):
+        r = requests.get('http://localhost:8000/api/bank/bank1/entry/entry1')
+        self.assertEqual(r.status_code, 200)
+        r = r.json()
+        self.assertEqual(r["status"], "ok")
+        self.assertEqual(r["data"], "value 1")
+
+    def test_003_keys(self):
+        r = requests.post('http://localhost:8000/api/bank/bank1/entry/entry1',
+                          headers={"Content-Type": "text / plain"},
+                          data="value 1")
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.post('http://localhost:8000/api/bank/bank1/entry/entry2',
+                          headers={"Content-Type": "text / plain"},
+                          data="value 2")
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.get('http://localhost:8000/api/bank/bank1/entries')
+        self.assertEqual(r.status_code, 200)
+        r = r.json()
+        self.assertEqual(r["status"], "ok")
+        self.assertIn("entry1", r["data"])
+        self.assertIn("entry2", r["data"])
+
+    def test_004_clear_bank(self):
+        r = requests.put('http://localhost:8000/api/bank/bank1?operation=reset')
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.get('http://localhost:8000/api/bank/bank1/entries')
+        self.assertEqual(r.status_code, 200)
+        r = r.json()
+
+        self.assertEqual(r["status"], "ok")
+        self.assertNotIn("entry1", r["data"])
+        self.assertNotIn("entry2", r["data"])
+
+    def test_005_banks(self):
+        r = requests.post('http://localhost:8000/api/bank/bank1/entry/entry1',
+                          headers={"Content-Type": "text / plain"},
+                          data="value 1")
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.post('http://localhost:8000/api/bank/bank2/entry/entry1',
+                          headers={"Content-Type": "text / plain"},
+                          data="value 1")
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.get('http://localhost:8000/api/banks')
+        self.assertEqual(r.status_code, 200)
+        r = r.json()
+
+        self.assertEqual(r["status"], "ok")
+        self.assertIn("bank1", r["data"])
+        self.assertIn("bank2", r["data"])
+
+    def test_005_error_get_unknownEntry(self):
+        r = requests.get('http://localhost:8000/api/bank/bank1/entry/unknownEntry')
+        self.assertEqual(r.status_code, 400)
+
+    def test_006_error_get_unknownBank(self):
+        r = requests.get('http://localhost:8000/api/bank/unknownBank/entry/unknownEntry')
+        self.assertEqual(r.status_code, 400)
+
+    def test_007_entries_unkwnowBank(self):
+        r = requests.get('http://localhost:8000/api/bank/unknownBank/entries')
+        self.assertEqual(r.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
